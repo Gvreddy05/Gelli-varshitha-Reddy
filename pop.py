68b4f8d637bbca75315355decd84962f3a3137b3
Gvreddy05 <gellivarshita05@gmail.com>
2024-07-25 13:47:24 +0530

    Add files via upload

---
+++ pop.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 23 10:31:51 2024
+
+@author: gelli
+"""
+
+from collections import deque
+numbers=[1,2,3,4]
+numbers=deque(numbers)
+numbers.pop()
+print(numbers)
---
+++ swp.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 22 11:53:05 2024
+
+@author: gelli
+"""
+
+x=5
+y=7
+print(x)
+print(y)
+x=x+y
+y=x-y
+x=x-y
+print(x)
+print(y)
